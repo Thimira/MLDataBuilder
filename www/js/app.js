479d44447ff48bd497b5e425c99b17e8bf6fe052
Thimira Amaratunga <thimiraamara@gmail.com>
2018-01-12 15:17:36 +0530

    Upgrading to Framework7 v2.0.6

--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,43 @@
-var pictureSource;   // available image sources
+/**
+ * Global variables
+ */
+var pictureSource; // available image sources
 var destinationType; // available formats of the returned images
 var imagePath; // the URI of the current image
 
-// Initialize app
-var myApp = new Framework7({
-    material: true,
-    pushState: true // device back button will bring you to the main page
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: 'left',
+    },
+    // Add default routes
+    routes: [{
+        path: '/about/',
+        url: 'about.html',
+    }, {
+        path: '/labels/',
+        url: 'labels.html',
+    }, ],
+    view: {
+        pushState: true, // device back button will bring you to the main page
+    }
 });
 
+var mainView = app.views.create('.view-main');
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add view
-var mainView = myApp.addView('.view-main', {
-    dynamicNavbar: false
-});
-
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
+$$(document).on('deviceReady', function() {
     console.log("Device is ready!");
 
-    // myApp.alert('Device is ready!');
     pictureSource = navigator.camera.PictureSourceType;
     destinationType = navigator.camera.DestinationType;
 
@@ -32,16 +48,16 @@ $$(document).on('deviceready', function() {
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+// myApp.onPageInit('about', function (page) {
+//     // Do something here for "about" page
 
-});
+// });
 
-myApp.onPageInit('index', function (page) {
-    // Do something here for "about" page
+// myApp.onPageInit('index', function (page) {
+//     // Do something here for "about" page
 
-    setInitialImage();
-})
+//     setInitialImage();
+// })
 
 /**
  * Sets the 'No Image' icon to the image placeholder
